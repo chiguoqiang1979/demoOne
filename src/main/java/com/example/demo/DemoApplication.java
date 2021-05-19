93537e79f7730967d2d9bf514eb2f26e02586195
zld19970115 <249766021@qq.com>
2021-05-19 11:08:14 +0800

    demo for jinkins

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("demo for jinkins ... ..."+System.currentTimeMillis());
+        System.out.println("这是第一次 @_@");
     }
 
 }
